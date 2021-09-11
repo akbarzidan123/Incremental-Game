17a4243cf7a67f7a7079c36c30428db771ce3f30
akbar zidan <akbarzidan123@gmail.com>
2021-09-11 15:58:52 +0700

    Incremental-Game

---
+++ Incremental_Game/Incremental Game/Assets/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static AudioClip coinSound;
+    static AudioSource audioSource;
+    void Start()
+    {
+        // coinSound = Load<AudioClip>("Coin");
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public static void PlayCoin()
+    {
+        audioSource.Play();
+    }
+}
